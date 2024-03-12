a7d581272f1a97e24842855739d3db745be56061
Md Mehedi Hasan <116740305+DevMehedi-CE@users.noreply.github.com>
2024-03-12 20:49:52 +0600

    create pages & routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,33 @@
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import SingIn from "./pages/SingIn";
+import SingUp from "./pages/SingUp";
+import Projects from "./pages/Projects";
+import Dashboard from "./pages/Dashboard";
 
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+    
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/sing-in" element={<SingIn/>} />
+          <Route path="/sing-up" element={<SingUp/>} />
+          <Route path="/projects" element={<Projects/>} />
+          <Route path="/dashboard" element={<Dashboard/>} />
+      
+       
+      </Route>
+    )
+  );
 
   return (
-   <div className="text-center">
-   <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-   
-   </div>
+    <>
+    <RouterProvider router={router} />
+    </>
   )
 }
 
