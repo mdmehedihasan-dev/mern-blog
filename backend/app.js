d1366f2de95ff2b2ed5c823724916b980e4a5e2a
Md Mehedi Hasan <116740305+DevMehedi-CE@users.noreply.github.com>
2024-03-13 17:06:15 +0600

    create a test api route

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,19 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRoutes from './routes/user.route.js'
 
+const app = express();
 dotenv.config()
-
 mongoose.connect(process.env.MONGODB_URL).then(()=>{
     console.log("Connected to MongoDB😊")
 }).catch((err)=>{
     console.log(err)
 })
 
-const app = express();
+
+
+app.use('/api/user',userRoutes)
 
 
 
