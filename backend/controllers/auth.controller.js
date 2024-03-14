3a53c59e7229e049d90b375c8779219e40e26429
Md Mehedi Hasan <116740305+DevMehedi-CE@users.noreply.github.com>
2024-03-15 01:06:38 +0600

    create signin api route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from "../utils/error.js";
+import  jwt  from "jsonwebtoken";
+
+// sing-up api 
 export const singup =async (req,res,next)=>{
     const {username, email, password} = req.body;
 
@@ -23,6 +26,41 @@ export const singup =async (req,res,next)=>{
     }
 
     
+
+
+}
+
+// sing-in api 
+
+export const signin = async(req,res,next)=>{
+    const {email,password} = req.body
+    if(!email || !password || email==="" || password===""){
+       return next(errorHandler(400,"All Fields are required🥴"))
+    }
+
+    try {
+        const validUser = await User.findOne({email});
+        if(!validUser){
+         return   next(errorHandler(404,"User not found😥"))
+        }
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+        if(!validPassword){
+          return  next(errorHandler(400,"Password is incorrect😟"))
+        }
+        const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+        const {password:pass,...rest}=validUser._doc;
+        res.status(200).cookie('access_token',token,{
+         httpOnly:true    
+        }).json(rest)
+        
+
+    } catch (error) {
+        next(error)
+        
+    }
+
+
 
 
 }
