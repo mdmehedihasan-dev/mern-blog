40195a3b8fd5886727f65820b1d6df73bc41b409
Md Mehedi Hasan <116740305+DevMehedi-CE@users.noreply.github.com>
2024-03-17 21:36:32 +0600

    make dashboard component private route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
@@ -6,46 +6,83 @@ import SignUp from "./pages/SignUp";
 import Projects from "./pages/Projects";
 import Dashboard from "./pages/Dashboard";
 import Main from "./layout/Main";
-import Footerr from "./components/Footerr";
+import PrivateRoute from "./components/PrivateRoute";
+
+
+
+
+
+
+
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main />,
 
-      children: [
-        {
-          path: "/",
-          element: <Home />,
-        },
-        {
-          path: "/about",
-          element: <About />,
-        },
-        {
-          path: "/sign-in",
-          element: <SignIn />,
-        },
-        {
-          path: "/sign-up",
-          element: <SignUp />,
-        },
-        {
-          path: "/projects",
-          element: <Projects />,
-        },
-        {
-          path: "/dashboard",
-          element: <Dashboard />,
-        },
-        {
-          path: "/footer",
-          element: <Footerr />,
-        },
-      ],
-    },
-  ]);
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+  
+      
+
+        <Route path="/" element={<Main/>}>
+      <Route  path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>} />
+      <Route  path="/sign-in" element={<SignIn/>} />
+      <Route  path="/sign-up" element={<SignUp/>} />
+
+      <Route element={<PrivateRoute/>} >
+
+      <Route  path="/dashboard" element={<Dashboard/>} />
+      </Route>
+
+
+      <Route  path="/projects" element={<Projects/>} />
+
+      
+
+      
+ 
+
+       
+
+
+      </Route>
+    )
+  );
+  //   {
+  //     path: "/",
+  //     element: <Main />,
+
+  //     children: [
+  //       {
+  //         path: "/",
+  //         element: <Home />,
+  //       },
+  //       {
+  //         path: "/about",
+  //         element: <About />,
+  //       },
+  //       {
+  //         path: "/sign-in",
+  //         element: <SignIn />,
+  //       },
+  //       {
+  //         path: "/sign-up",
+  //         element: <SignUp />,
+  //       },
+  //       {
+  //         path: "/projects",
+  //         element: <Projects />,
+  //       },
+  //       {
+  //         path: "/dashboard",
+  //         element: <Dashboard />,
+  //       },
+  //       {
+  //         path: "/footer",
+  //         element: <Footerr />,
+  //       },
+  //     ],
+  //   },
+  // ]);
   return (
     <>
       <RouterProvider router={router} />
