26d013be032c5b474410276aa60740ea59d12cc9
Md Mehedi Hasan <116740305+DevMehedi-CE@users.noreply.github.com>
2024-03-20 17:46:45 +0600

    update UserApi Route

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,11 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
+import cookieParser from 'cookie-parser'
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 dotenv.config()
 mongoose.connect(process.env.MONGODB_URL).then(()=>{
     console.log("Connected to MongoDB😊")
